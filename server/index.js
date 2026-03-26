775a05de780313825d96f6f6e35fb443058d98fb
[Sujal Wadhankar] <[sujalwadhankar938@gmail.com]>
2026-03-26 11:55:22 +0530

    fix port

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const { connect } = require('./db');
 const router = require("./Routes.js/index");
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyparser.json({ limit: '50mb' }));
 app.use(bodyparser.urlencoded({ limit: '50mb', extended: true }));
